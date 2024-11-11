ba1c7c105ca5b93097e765bae435932e60d892bd
Manuel de la Peña <mdelapenya@gmail.com>
2024-11-11 18:01:35 +0100

    feat: first example using ollama and langchain go

---
+++ 01-hello-world/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/testcontainers/testcontainers-go"
+	tcollama "github.com/testcontainers/testcontainers-go/modules/ollama"
+	"github.com/tmc/langchaingo/llms"
+	"github.com/tmc/langchaingo/llms/ollama"
+)
+
+func main() {
+	if err := run(); err != nil {
+		log.Fatalf("run: %s", err)
+	}
+}
+
+func run() (err error) {
+	c, err := tcollama.Run(context.Background(), "ilopezluna/llama3.2:0.3.13-1b")
+	if err != nil {
+		return err
+	}
+	defer func() {
+		err = testcontainers.TerminateContainer(c)
+		if err != nil {
+			err = fmt.Errorf("terminate container: %w", err)
+		}
+	}()
+
+	ollamaURL, err := c.ConnectionString(context.Background())
+	if err != nil {
+		return fmt.Errorf("connection string: %w", err)
+	}
+
+	llm, err := ollama.New(ollama.WithModel("llama3.2:1b"), ollama.WithServerURL(ollamaURL))
+	if err != nil {
+		return fmt.Errorf("ollama new: %w", err)
+	}
+
+	ctx := context.Background()
+	content := []llms.MessageContent{
+		llms.TextParts(llms.ChatMessageTypeSystem, "You are a fellow Go developer."),
+		llms.TextParts(llms.ChatMessageTypeHuman, "Provide 3 short bullet points explaining why Go is awesome"),
+	}
+
+	completion, err := llm.GenerateContent(ctx, content)
+	if err != nil {
+		return fmt.Errorf("llm generate content: %w", err)
+	}
+
+	for _, choice := range completion.Choices {
+		fmt.Println(choice.Content)
+	}
+
+	return nil
+}
---
+++ README.md
@@ -0,0 +1,29 @@
+# Generative AI with Testcontainers for Golang
+
+This project demonstrates how to use Testcontainers for Golang to create a seamless development environment for building Generative AI applications.
+
+## Project Structure
+
+1. [`01-hello-world`](./01-hello-world): Contains a simple example of using a language model to generate text.
+
+## Prerequisites
+
+- Go 1.23 or higher
+- Docker
+
+## Setup
+
+1. Clone the repository:
+    ```sh
+    git clone https://github.com/mdelapenya/generative-ai-testcontainers.git
+    cd generative-ai-testcontainers
+    ```
+
+## Running the Examples
+
+To run the examples, navigate to the desired directory and run the `go run .` command. For example, to run the `1-hello-world` example:
+
+```sh
+cd 1-hello-world
+go run .
+```
