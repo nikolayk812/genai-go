f234839c2060ae3ea02f8aee748b934ace0d3285
Manuel de la Peña <mdelapenya@gmail.com>
2025-01-17 11:45:22 +0530

    chore: include vectors db images in the pull

--- pull-images.sh
+++ pull-images.sh
@@ -5,3 +5,6 @@ docker pull mdelapenya/llama3.2:0.5.4-3b
 docker pull mdelapenya/qwen2:0.5.4-0.5b
 docker pull mdelapenya/moondream:0.5.4-1.8b
 docker pull mdelapenya/all-minilm:0.5.4-22m
+
+docker pull semitechnologies/weaviate:1.27.2
+docker pull pgvector/pgvector:pg16
