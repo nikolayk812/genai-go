901b52718b5a56c634bca3d03bc60f7b0559ccfe
Manuel de la Peña <mdelapenya@gmail.com>
2025-01-23 17:13:58 +0800

    chore: pull images in parallel, as ollama will create the base layer that will be cached for the rest

--- pull-images.sh
+++ pull-images.sh
@@ -1,12 +1,14 @@
 #! /bin/bash
 
-docker pull testcontainers/ryuk:0.11.0
+docker pull ollama/ollama:0.5.4
 
-docker pull mdelapenya/llama3.2:0.5.4-1b
-docker pull mdelapenya/llama3.2:0.5.4-3b
-docker pull mdelapenya/qwen2:0.5.4-0.5b
-docker pull mdelapenya/moondream:0.5.4-1.8b
-docker pull mdelapenya/all-minilm:0.5.4-22m
+docker pull testcontainers/ryuk:0.11.0 &
+docker pull mdelapenya/llama3.2:0.5.4-1b &
+docker pull mdelapenya/llama3.2:0.5.4-3b &
+docker pull mdelapenya/qwen2:0.5.4-0.5b &
+docker pull mdelapenya/moondream:0.5.4-1.8b &
+docker pull mdelapenya/all-minilm:0.5.4-22m &
+docker pull semitechnologies/weaviate:1.27.2 &
+docker pull pgvector/pgvector:pg16 &
 
-docker pull semitechnologies/weaviate:1.27.2
-docker pull pgvector/pgvector:pg16
+wait
