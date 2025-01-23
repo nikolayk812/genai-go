26929f0ee147808906354d7c41005884bb615351
Manuel de la Peña <mdelapenya@gmail.com>
2025-01-23 17:14:37 +0800

    chore: extract ollama version to a constant in pull script

--- pull-images.sh
+++ pull-images.sh
@@ -1,13 +1,15 @@
 #! /bin/bash
 
-docker pull ollama/ollama:0.5.4
+readonly OLLAMA_VERSION="0.5.4"
+
+docker pull ollama/ollama:${OLLAMA_VERSION}
 
 docker pull testcontainers/ryuk:0.11.0 &
-docker pull mdelapenya/llama3.2:0.5.4-1b &
-docker pull mdelapenya/llama3.2:0.5.4-3b &
-docker pull mdelapenya/qwen2:0.5.4-0.5b &
-docker pull mdelapenya/moondream:0.5.4-1.8b &
-docker pull mdelapenya/all-minilm:0.5.4-22m &
+docker pull mdelapenya/llama3.2:${OLLAMA_VERSION}-1b &
+docker pull mdelapenya/llama3.2:${OLLAMA_VERSION}-3b &
+docker pull mdelapenya/qwen2:${OLLAMA_VERSION}-0.5b &
+docker pull mdelapenya/moondream:${OLLAMA_VERSION}-1.8b &
+docker pull mdelapenya/all-minilm:${OLLAMA_VERSION}-22m &
 docker pull semitechnologies/weaviate:1.27.2 &
 docker pull pgvector/pgvector:pg16 &
 
