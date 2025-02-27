fccecdaf8eca16594e4e1e6ad51d27f765fd41d0
Nikolay Kuznetsov <nikolay.kuznetsov@zalando.fi>
2025-02-27 18:24:03 +0200

    01: remove tc and openai

--- 01-hello-world/main.go
+++ 01-hello-world/main.go
@@ -3,48 +3,35 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
-
-	"github.com/testcontainers/testcontainers-go"
-	tcollama "github.com/testcontainers/testcontainers-go/modules/ollama"
 	"github.com/tmc/langchaingo/llms"
 	"github.com/tmc/langchaingo/llms/ollama"
+	"log"
 )
 
 func main() {
-	if err := run(); err != nil {
+	ctx := context.Background()
+
+	if err := run(ctx); err != nil {
 		log.Fatalf("run: %s", err)
 	}
 }
 
-func run() (err error) {
-	c, err := tcollama.Run(context.Background(), "mdelapenya/llama3.2:0.5.4-1b", testcontainers.CustomizeRequest(testcontainers.GenericContainerRequest{
-		ContainerRequest: testcontainers.ContainerRequest{
-			Name: "chat-model",
-		},
-		Reuse: true,
-	}))
+func run(ctx context.Context) error {
+	llm, err := ollama.New(
+		ollama.WithModel("llama3.2"),
+		ollama.WithServerURL("http://localhost:11434"))
 	if err != nil {
-		return err
-	}
-	defer func() {
-		err = testcontainers.TerminateContainer(c)
-		if err != nil {
-			err = fmt.Errorf("terminate container: %w", err)
-		}
-	}()
-
-	ollamaURL, err := c.ConnectionString(context.Background())
-	if err != nil {
-		return fmt.Errorf("connection string: %w", err)
+		return fmt.Errorf("ollama.New: %w", err)
 	}
 
-	llm, err := ollama.New(ollama.WithModel("llama3.2:1b"), ollama.WithServerURL(ollamaURL))
-	if err != nil {
-		return fmt.Errorf("ollama new: %w", err)
-	}
+	//llm, err := openai.New(
+	//	openai.WithModel("gpt-4"),
+	//	openai.WithToken(os.Getenv("OPENAI_API_KEY")),
+	//)
+	//if err != nil {
+	//	return fmt.Errorf("openai.New: %w", err)
+	//}
 
-	ctx := context.Background()
 	content := []llms.MessageContent{
 		llms.TextParts(llms.ChatMessageTypeSystem, "You are a fellow Go developer."),
 		llms.TextParts(llms.ChatMessageTypeHuman, "Provide 3 short bullet points explaining why Go is awesome"),
@@ -53,7 +40,7 @@ func run() (err error) {
 	// The response from the model happens when the model finishes processing the input, which it's usually slow.
 	completion, err := llm.GenerateContent(ctx, content)
 	if err != nil {
-		return fmt.Errorf("llm generate content: %w", err)
+		return fmt.Errorf("llm.GenerateContent: %w", err)
 	}
 
 	for _, choice := range completion.Choices {
